11b0557ec7c6a003f890e74efc9ebd4dc57e9746
Spagett41 <erik.hedlund6@gmail.com>
2024-11-26 11:59:15 +0100

    Fixed stats to include game rules and not exceed thesholds

---
+++ PokemonBattle/HealthStat.cs
@@ -0,0 +1,28 @@
+using PokemonBattle.Interfaces;
+
+namespace PokemonBattle
+{
+    public class HealthStat : IStat
+    {
+        public int DefaultStat { get; private set; }
+        public int CurrentStat { get; private set; }
+
+        public HealthStat(int stat)
+        {
+            DefaultStat = stat;
+            CurrentStat = stat;
+        }
+
+        // Updates health and makes sure it go below 0 or the default (max) value
+        public void UpdateStat(int change)
+        {
+            CurrentStat = change > 0 ? Math.Min(DefaultStat, CurrentStat + change) : Math.Max(0, CurrentStat - change);
+        }
+
+        // Resets to default value
+        public void ResetStat()
+        {
+            CurrentStat = DefaultStat;
+        }
+    }
+}
