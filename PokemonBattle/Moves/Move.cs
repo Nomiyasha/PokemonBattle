3c54a057f2395aa29d95ce7c133c964fc8bac987
Spagett41 <erik.hedlund6@gmail.com>
2024-11-27 14:36:39 +0100

    Updated moves to handle specific case with both user and target

---
+++ PokemonBattle/Moves/Move.cs
@@ -0,0 +1,25 @@
+﻿using PokemonBattle.Enums;
+
+namespace PokemonBattle.Moves
+{
+    public abstract class Move : IMove
+    {
+        public bool IsSelfTarget { get; }
+        public int Change { get; }
+        public Elements Type { get; }
+        public string Name { get; }
+        public string Description { get; }
+
+        public Move(bool isSelfTarget, int change, Elements type, string name, string description)
+        {
+            IsSelfTarget = isSelfTarget;
+            Change = change; 
+            Type = type; 
+            Name = name; 
+            Description = description;
+        }
+
+        public virtual void Use(Pokemon target) { }
+        public virtual void Use(Pokemon user, Pokemon target) { }
+    }
+}
