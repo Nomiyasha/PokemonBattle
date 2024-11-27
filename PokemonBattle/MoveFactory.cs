1f9c49d687d1f65f02d1d6c94347a0205a97af79
Waddlingabout <ejm-nylander@outlook.com>
2024-11-27 17:43:01 +0100

    Factory klar Råkade arbeta direkt i main :P

---
+++ PokemonBattle/MoveFactory.cs
@@ -0,0 +1,31 @@
+﻿using PokemonBattle.Enums;
+using PokemonBattle.Moves;
+
+namespace PokemonBattle;
+
+public class MoveFactory
+{
+    /// <summary>
+    /// Returns a IMove object based on inputed string.
+    /// </summary>
+    /// <param name="moveName">Accepted Entries:
+    /// scratch, tackle, vinewhip, ember, watergun, growl, growth, withdraw, tailwhip</param>
+    /// <returns>A IMove object</returns>
+    /// <exception cref="ArgumentException">Invalid name entered.</exception>
+    public IMove Create(string moveName)
+    {
+        return moveName.ToLower() switch
+        {
+            "scratch" => new HealthMove(false, -40, "Scratch", "Scratches with sharp claws.", Elements.Normal), 
+            "tackle" => new HealthMove(false, -40, "Tackle", "A full-body charge attack.", Elements.Normal),
+            "vinewhip" => new HealthMove(false, -40, "Vine Whip", "Whips the foe with slender vines.", Elements.Grass),
+            "ember" => new HealthMove(false, -40, "Ember", "An attack that may inflict a burn.", Elements.Fire),
+            "watergun" => new HealthMove(false, -40, "Watergun", "Squirts water to attack.", Elements.Water),
+            "growl" => new AttackMove(false, -1, "Growl", "Reduces the foe's Attack.", Elements.Normal),
+            "growth" => new AttackMove(true, 1, "Growth", "The user's body grows all at once, raising the Attack stat.", Elements.Normal),
+            "withdraw" => new DefenceMove(false, 1, "Withdraw", "Withdraws the body into its hard shell to raise Defense..", Elements.Water),
+            "tailwhip" => new DefenceMove(true, -1, "Tail Whip", "Lowers the foe's Defense.", Elements.Normal),
+            _ => throw new ArgumentException("Invalid moveName")
+        };
+    }
+}
--- PokemonBattle/Program.cs
+++ PokemonBattle/Program.cs
@@ -9,16 +9,16 @@ class Program
     {
         Console.CursorVisible = false;
         Console.OutputEncoding = System.Text.Encoding.UTF8;
-        
+        MoveFactory factory = new MoveFactory();
         IMove[] moves = [
-            MovesLib.healthMoves["Scratch"],
-            MovesLib.healthMoves["Scratch"],
-            MovesLib.healthMoves["Scratch"],
-            MovesLib.healthMoves["Scratch"]
+            factory.Create("ember"),
+            factory.Create("scratch"),
+            factory.Create("tailwhip"),
+            factory.Create("growl"),
         ];
         
-        Pokemon p = new Pokemon("Charizard", 220, moves.ToList(), Elements.Fire);
-        Pokemon enemy = new Pokemon("Test", 120, moves.ToList(), Elements.Grass);
+        Pokemon p = new Pokemon("Charmander", new HealthStat(50), moves.ToList(), Elements.Fire);
+        Pokemon enemy = new Pokemon("Squirtle", new HealthStat(50), moves.ToList(), Elements.Grass);
 
         Battle battle = new Battle(p,enemy);
     }
