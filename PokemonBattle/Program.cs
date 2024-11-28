afa3a1bccc887054f33e646d4af61c99e2c44fc2
Spagett41 <erik.hedlund6@gmail.com>
2024-11-28 11:54:35 +0100

    Updated stats to calculate on level

--- PokemonBattle/Program.cs
+++ PokemonBattle/Program.cs
@@ -16,9 +16,10 @@ class Program
             factory.Create("tailwhip"),
             factory.Create("growl"),
         ];
-        
-        Pokemon p = new Pokemon("Charmander", new HealthStat(50), new AttackDefenceStat(77), new AttackDefenceStat(77), moves.ToList(), Elements.Fire);
-        Pokemon enemy = new Pokemon("Squirtle", new HealthStat(50), new AttackDefenceStat(77), new AttackDefenceStat(77), moves.ToList(), Elements.Grass);
+        Level lv5 = new Level(5);
+        Level lv10 = new Level(10);
+        Pokemon p = new Pokemon("Charmander", new HealthStat(50, lv10), lv10, new AttackDefenceStat(48, lv10), new AttackDefenceStat(48, lv10), moves.ToList(), Elements.Fire);
+        Pokemon enemy = new Pokemon("Squirtle", new HealthStat(50, lv5), lv5, new AttackDefenceStat(48, lv5), new AttackDefenceStat(48, lv5), moves.ToList(), Elements.Grass);
 
         Battle battle = new Battle(p,enemy);
     }
